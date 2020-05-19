8592fe665a8aaddf68cfd3226c759bfd9ec63f02
kosuke0320 <63537539+kosuke0320@users.noreply.github.com>
2020-05-20 08:40:03 +0900

    Update helper.rb

--- helper.rb
+++ helper.rb
@@ -3,9 +3,10 @@
 INTERNAL = "./internal/"
 ARTICLE = "#{INTERNAL}/article/"
 DATAS = "#{INTERNAL}/data/"
+OPERATIONS = "#{INTERNAL}/operations/"
 
 # ls(path) returns all file's name in 'path'
-def ls(path) 
+def ls(path)
   ary = Array.new
   Dir.chdir(path) {
     Dir.glob("*").each {|dir|
